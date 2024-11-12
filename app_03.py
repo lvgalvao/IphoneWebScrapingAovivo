443c4fb6111f05703501f07c8c503e7da9411628
Luciano Vasconcelos <lvgalvaofilho@gmail.com>
2024-11-12 20:59:14 -0300

    etapa 03

---
+++ app_03.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+def fetch_page():
+    url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287851#polycard_client=search-nordic&wid=MLB5054621110&sid=search&searchVariation=MLB1040287851&position=2&search_layout=stack&type=product&tracking_id=a2201312-8133-4041-83eb-9ef4bec42f10"
+    response = requests.get(url)
+    return response.text
+
+def parse_page(html):
+    soup = BeautifulSoup(html,'html.parser')
+    product_name = soup.find('h1', class_='ui-pdp-title').get_text()
+    prices: list = soup.find_all('span', class_='andes-money-amount__fraction')
+    old_price: int = int(prices[0].get_text().replace('.', ''))
+    new_price: int = int(prices[1].get_text().replace('.', ''))
+    installment_price: int = int(prices[2].get_text().replace('.', ''))
+
+    return {
+        'product_name': product_name,
+        'old_price': old_price,
+        'new_price': new_price,
+        'installment_price': installment_price
+    }
+
+if __name__ == "__main__":
+    while True:
+        page_content = fetch_page()
+        produto_info = parse_page(page_content)
+        print(produto_info)
+        time.sleep(10)
